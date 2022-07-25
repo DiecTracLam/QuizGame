2711416784ca6a3b608e0821b04e79c0ebeb37e6
Diec Trac Lam <diectraclam.0812@gmail.com>
2022-07-25 22:27:44 +0700

    Add cors

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const router = require('./routes');
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(bodyParser.json({ extended: true }));
+app.use('/',router)
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
     connectDB();
 })
 
-app.use('/',router)
-app.use(cors())
